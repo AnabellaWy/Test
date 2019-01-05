94b3eadc234ea9f9d7c006d0ee0911e7088be6ea
wy <1150471222@qq.com>
2019-01-05 14:58:39 +0800

    创建本地项目，往远程仓库推送

---
+++ src/main/java/cn/bdqn/tangcco/tcmp068/wangying/gittest/GittestApplication.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp068.wangying.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GittestApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/cn/bdqn/tangcco/tcmp068/wangying/gittest/GittestApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.bdqn.tangcco.tcmp068.wangying.gittest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GittestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
